3b5f28ef7deea3036063c5f10f1b4f9475cbf7b4
Michael Klein <miketheklein@gmail.com>
2018-08-14 13:59:01 -0700

    created sqllite database for native storage of articles

--- news/lib/src/models/item_models.dart
+++ news/lib/src/models/item_models.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
-  final  String type;
+  final String type;
   final String by;
   final int time;
   final String text;
@@ -27,4 +29,37 @@ class ItemModel {
         score = parsedJson['score'],
         title = parsedJson['title'],
         descendants = parsedJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        deleted = parsedJson['deleted'] == 1,
+        type = parsedJson['type'],
+        by = parsedJson['by'],
+        time = parsedJson['time'],
+        text = parsedJson['text'],
+        dead = parsedJson['dead'] == 1,
+        parent = parsedJson['parent'],
+        kids = jsonDecode(parsedJson['kids']),
+        url = parsedJson['url'],
+        score = parsedJson['score'],
+        title = parsedJson['title'],
+        descendants = parsedJson['descendants'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic>{
+      'id': id,
+      'type': type,
+      'by': by,
+      'time': time,
+      'text': text,
+      'parent': parent,
+      'url': url,
+      'score': score,
+      'title': title,
+      'descendants': descendants,
+      'deleted': deleted ? 1 : 0,
+      'dead': dead ? 1 : 0,
+      'kids': jsonEncode(kids),
+    };
+  }
 }
